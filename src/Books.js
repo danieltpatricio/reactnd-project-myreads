ab1bd7003c3d02ca813fab7a025f20fbd2211dd8
Daniel Patricio <danieltp123@hotmail.com>
2018-10-02 21:01:29 -0300

    init the project

---
+++ src/Books.js
@@ -0,0 +1,45 @@
+books ={
+    1: {
+        id:1,
+        title:"To Kill a Mockingbird",
+        author:"Harper Lee",
+        state:""
+    },
+    2: {
+        id:2,
+        title:"Ender's Game",
+        author:"Orson Scott Card",
+        state:""
+    },
+    3: {
+        id:3,
+        title:"1776",
+        author:"David McCullough",
+        state:""
+    },
+    
+    4: {
+        id:4,
+        title:"Harry Potter and the Sorcerer's Stone",
+        author:"J.K. Rowling",
+        state:""
+    },
+    5: {
+        id:5,
+        title:"The Hobbit",
+        author:"J.R.R. Tolkien",
+        state:""
+    },
+    6: {
+        id:6,
+        title:"Oh, the Places You'll Go!",
+        author:"Seuss",
+        state:""
+    },
+    7: {
+        id:7,
+        title:"The Adventures of Tom Sawyer",
+        author:"Mark Twain",
+        state:""
+    }
+};
